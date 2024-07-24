f6085fb79f56f51d7134bd439938a47fbf90c553
farhan <farhanj.mohamed@gmail.com>
2024-07-24 12:41:44 -0700

    added link to profile from project

--- src/Splash.jsx
+++ src/Splash.jsx
@@ -3,7 +3,9 @@ export function Splash() {
     <div className="w-screen h-auto min-h-screen animated-gradient">
       <p className=" text-white text-[10rem] text-center pt-[20rem] font-sans font-black ">
         <a href="/games">JS FUN BOX</a>
-        <p className="text-sm">By Farhan J Mohamed</p>
+        <p className="text-sm">
+          <a href="https://farhanjmohamed.com/">By Farhan J Mohamed</a>
+        </p>
       </p>
     </div>
   );
